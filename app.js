f8cee33618e452850423952b05124c18dc5ce1ca
Yoshi-purple <n7ydahewgmail.com>
2020-12-09 03:46:09 +0900

    文字を追加できる機能を追加しました。

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
-"use strict";
+'use strict';
 
 {
   // selectors
-  const todoInput = document.querySelector(".todo-input");
-  const todoButton = document.querySelector(".todo-button");
-  const todoList = document.querySelector(".todo-list");
+  const todoInput = document.querySelector('.todo-input');
+  const todoButton = document.querySelector('.todo-button');
+  const todoList = document.querySelector('.todo-list');
   // eventlisteners
-  todoButton.addEventListener("click", addTodo);
+  todoButton.addEventListener('click', addTodo);
   // functions
   function addTodo(event) {
     event.preventDefault();
 
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
-
-    const newTodo = document.createElement("li");
-    newTodo.innerText = "hey";
-    newTodo.classList.add(".todo-item");
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('.todo-item');
     todoDiv.appendChild(newTodo);
+    //checkmarkbutton
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    //trashmarkbutton
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    // append list
+    todoList.appendChild(todoDiv);
+    todoInput.value = '';
   }
 }
