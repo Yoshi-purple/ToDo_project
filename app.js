09cfb20c2cacc1b5084b38cfcd07a4232f4b5830
Yoshi-purple <n7ydahew@gmail.com>
2020-12-09 17:09:01 +0900

    絞り込みのアイテムを追加（動画53:00～）

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/** @format */
+
 'use strict';
 
 {
@@ -5,8 +7,11 @@
   const todoInput = document.querySelector('.todo-input');
   const todoButton = document.querySelector('.todo-button');
   const todoList = document.querySelector('.todo-list');
+  const filterOption = document.querySelector('.filter-todo');
   // eventlisteners
   todoButton.addEventListener('click', addTodo);
+  todoList.addEventListener('click', deleteCheck);
+  filterOption.addEventListener('click', filterTodo);
   // functions
   function addTodo(event) {
     event.preventDefault();
@@ -31,5 +36,40 @@
     // append list
     todoList.appendChild(todoDiv);
     todoInput.value = '';
+  }
+  function deleteCheck(e) {
+    const item = e.target;
+    // delete todo
+    if (item.classList[0] === 'trash-btn') {
+      const todo = item.parentElement;
+      // annimation
+      todo.classList.add('fall');
+      todo.addEventListener('transitionend', function () {
+        todo.remove();
+      });
+    }
+
+    // check mark
+    if (item.classList[0] === 'complete-btn') {
+      const todo = item.parentElement;
+      todo.classList.toggle('completed');
+    }
+  }
+
+  function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function (todo) {
+      switch (e.target.value) {
+        case 'all':
+          todo.style.display = 'flex';
+          break;
+        case 'completed':
+          if (todo.classList.contains('completed')) {
+            todo.style.display = 'flex';
+          } else {
+            todo.style.display = 'none';
+          }
+      }
+    });
   }
 }
